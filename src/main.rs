b6f5b59ee5b34fe810547404678ef2f60841281a
Marat Zabirov <marat.zabirov@namisoft.ru>
2020-09-25 23:47:41 +0300

    create blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use blockchainlib::*;
 fn main () {
-    println!("Hello, blockchain!");
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, now(), vec![0;32], 118318, "Genesis block".to_owned(), difficulty);
+
+    block.hash = block.hash();
+
+    println!("{:?}", &block);
+
+    block.mine();
+
+    println!("Mined genesis block: {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = BlockChain {
+        blocks: vec![block],
+    };
+
+    println!("Verify: {}", &blockchain.verify());
+
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, now(), last_hash, 0, "Another block".to_owned(), difficulty);
+        block.mine();
+
+        println!("{:?}", &block);
+        last_hash = block.hash.clone();
+        blockchain.blocks.push(block);
+        println!("Verify: {}", &blockchain.verify());
+    }
+
+    // blockchain.blocks[3].index = 6;
+    blockchain.blocks[3].hash = last_hash;
+    println!("Verify: {}", &blockchain.verify());
 }
